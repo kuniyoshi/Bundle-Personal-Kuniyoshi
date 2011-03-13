2cdf7e88e275060b3a35e2abb5109bed861cb032
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-03-13 14:20:36 +0900

    INIT.

---
+++ lib/Bundle/Personal/Kuniyoshi.pm
@@ -0,0 +1,44 @@
+package Bundle::Personal::Kuniyoshi;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+1;
+
+__END__
+=head1 NAME
+
+Bundle::Personal::Kuniyoshi - CPAN for me
+
+=head1 SYNOPSIS
+
+cpanm Bundle::Personal::Kuniyoshi
+
+=head1 CONTENTS
+
+Readonly
+
+Time::Piece
+
+DateTime
+
+=head1 DESCRIPTION
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+kuniyoshi kouji, E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by kuniyoshi kouji
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.12.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
