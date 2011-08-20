1bf3674b8ad0178ff2117fbdaef2c4dc9a6ccbb7
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-08-20 11:16:29 +0900

    Add a Module::Install.

--- lib/Bundle/Personal/Kuniyoshi.pm
+++ lib/Bundle/Personal/Kuniyoshi.pm
@@ -1,9 +1,8 @@
 package Bundle::Personal::Kuniyoshi;
-
 use strict;
 use warnings;
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 1;
 
@@ -17,6 +16,8 @@ Bundle::Personal::Kuniyoshi - CPAN for me
 cpanm Bundle::Personal::Kuniyoshi
 
 =head1 CONTENTS
+
+Module::Install
 
 YAML
 
